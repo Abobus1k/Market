4ceb0d56c2a5b33e53ba02d1dff087ac463bee3b
aobubu <ginkinvaginkin@gmail.com>
2023-11-09 16:02:00 +0100

    Add validation

--- target/generated-sources/annotations/ru/example/megamarket/category/CategoryMapperImpl.java
+++ target/generated-sources/annotations/ru/example/megamarket/category/CategoryMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-09T15:55:25+0100",
+    date = "2023-11-09T16:01:52+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 18.0.2-ea (Private Build)"
 )
 @Component
